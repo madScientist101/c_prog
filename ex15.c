167db625d1f8b32282467451ae0e5e5dadc34a01
michaeltetteh <miketetteh751@outlook.com>
2020-11-01 12:10:53 +0000

    pointers

---
+++ ex15.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int arr[10] = {3,6,9,12,15,18,21,32,43,65};
+
+    int *p = &arr[0];
+    for(int i=0; i<10;i++){
+        printf("%p  ==> %d \n",p+i,*(p+i));
+    }
+    return 0;
+}
